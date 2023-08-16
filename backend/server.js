27347a6cbec3abaaaab613b570635b0cdac453f9
athulraj10 <athulraj165@gmail.com>
2023-08-16 18:36:09 +0530

    api user added

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
+
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRouter)
 
