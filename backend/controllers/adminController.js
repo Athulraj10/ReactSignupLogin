d57df53b3fbd103dc76caa82174392675dc93153
Athulraj10 <athulraj165@gmail.com>
2023-08-20 21:35:19 +0530

    import admin panal side

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import Admin from "../models/adminModel.js";
+
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 // import { fetchAllUsers } from "../Helpers/adminHelpers.js";
