ed31e23d8e5815db320e53f1cbdc0899fc3edeca
Athulraj10 <athulraj165@gmail.com>
2023-08-20 21:36:05 +0530

    if any error accur in user login side

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,6 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
       email: user.email,
       image: user.imagePath?user.imagePath:null
     });
+    
   } else {
     res.status(401);
     throw new Error('Invalid email or password');
