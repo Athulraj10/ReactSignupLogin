4a5f22ef957d0abb42ecd5f173241f4a87c022da
Athulraj10 <athulraj165@gmail.com>
2023-08-25 20:24:55 +0530

    json file in user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,6 +85,8 @@ const logoutUser = (req, res) => {
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
   if (user) {
+
+
     
     res.json({
       _id: user._id,
