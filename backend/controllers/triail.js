c41f564ac9ef26e7c96338c16c5259b42c18f25b
Athulraj10 <athulraj165@gmail.com>
2023-08-26 15:55:52 +0530

    ds question 4

--- backend/controllers/triail.js
+++ backend/controllers/triail.js
@@ -24,3 +24,18 @@ for (let i = 0; i < arr.length; i++) {
 console.log(duplicateArray.sort());
 
 
+let arr = [1, 4, 2, 5, 7, 5, 6, 4, 4, 2];
+let duplicateArray = [];
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] === arr[j] ) {
+            duplicateArray.push(arr[i]);
+        }
+    }
+}
+
+console.log(duplicateArray);
+
+
+
