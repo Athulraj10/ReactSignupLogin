36967a3c61a4590f77600c1c80901452d4bb4ccc
Athulraj10 <athulraj165@gmail.com>
2023-08-26 15:54:55 +0530

    make visible

--- backend/controllers/triail.js
+++ backend/controllers/triail.js
@@ -1,11 +1,11 @@
-// let arr=[1,4,2,5,7,5,6,4,4,2];
-// let duplicateArray=[]
-// for(let i=0;i<arr.length;i++){
-//     for(let j=i+1;j<arr.length;j++){
-//         if(arr[i]==arr[j]){
-//             duplicateArray.push(arr[j])
-//         }
-//     }    
-// }
-// console.log(duplicateArray)
+let arr=[1,4,2,5,7,5,6,4,4,2];
+let duplicateArray=[]
+for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[i]==arr[j]){
+            duplicateArray.push(arr[j])
+        }
+    }    
+}
+console.log(duplicateArray)
 
